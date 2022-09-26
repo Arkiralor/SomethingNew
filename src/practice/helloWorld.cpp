ac99a7faecdb81049a43e3d3d5273502a7f615e5
Arkiralor <prithoo11335@gmail.com>
2022-09-26 07:51:13 +0530

    refactoring

--- src/practice/helloWorld.cpp
+++ src/practice/helloWorld.cpp
@@ -11,10 +11,36 @@ class HelloWorld
 private:
     std::string message, name;
 
+    const char *DEFAULT_NAME = "Prithoo";
+    const char *DEFAULT_MESSAGE = "Welcome to the world";
+
 public:
+    HelloWorld(){
+        this->name = this->DEFAULT_NAME;
+
+        this->message = this->DEFAULT_MESSAGE;
+    }
+    HelloWorld(std::string name, std::string message){
+        if (name!=""){
+            this->name = name;
+        }
+        else{
+            this->name=this->DEFAULT_NAME;
+        }
+        if (message!=""){
+            this->message = message;
+        }
+        else{
+            this->message = this->DEFAULT_MESSAGE;
+        }
+    }
     void enter_message(void);
     void enter_name(void);
     void display_message(void);
+    void __str__(){
+        std::cout<<"\nNAME:\t"<<this->name;
+        std::cout<<"\nMESSAGE:\t"<<this->message;
+    }
 };
 
 void hello_world_func()
@@ -22,9 +48,13 @@ void hello_world_func()
     /*
     Function to call the 'HelloWorld' class methods.
     */
-    HelloWorld hello_world;
-    hello_world.enter_name();
-    hello_world.enter_message();
+    
+    std::string message, name;
+    std::cout << "Enter your name:\t";
+    getline(std::cin, name);
+    std::cout << "Enter your message:\t";
+    getline(std::cin, message);
+    HelloWorld hello_world(name, message);
     hello_world.display_message();
 }
 
@@ -59,5 +89,5 @@ void HelloWorld::enter_name(void)
 
 void HelloWorld::display_message(void)
 {
-    std::cout << this->message << ", " << this->name << ".\n";
+    std::cout <<"Complete Message:\t" <<this->message << ", " << this->name << ".\n";
 }
